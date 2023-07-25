5684beef28fb40d1660a8d876f3c8271ebfab415
devwuu <jdw0477@gmail.com>
2023-07-25 14:29:46 +0900

    init project

--- src/main/java/com/example/springsecurityexam/controller/IndexController.java
+++ src/main/java/com/example/springsecurityexam/controller/IndexController.java
@@ -4,42 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
-    @GetMapping({"", "/"})
-    public String index(){
-        return "index";
+    @GetMapping("home")
+    public String home(){
+        return "home";
     }
 
-    @GetMapping("/user")
-    public @ResponseBody String user(){
-        return "user";
-    }
-
-    @GetMapping("/admin")
-    public @ResponseBody String admin(){
-        return "admin";
-    }
-
-    @GetMapping("/manager")
-    public @ResponseBody String manager(){
-        return "manager";
-    }
-
-    @GetMapping("/login")
-    public @ResponseBody String login(){
-        return "login";
-    }
-
-    @GetMapping("/join")
-    public @ResponseBody String join(){
-        return "join";
-    }
-
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "ok";
-    }
 }
